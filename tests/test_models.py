00afdc3c335d931aa5f0a7c507e5efc7d98d3f20
sibinski <jevgeni.shibinski@gmail.com>
2026-05-24 12:03:08 -0400

    Fresh commit

--- tests/test_models.py
+++ tests/test_models.py
@@ -104,3 +104,7 @@ class TestProductModel(unittest.TestCase):
     #
     # ADD YOUR TEST CASES HERE
     #
+    def test_read_a_product(self):
+        """It should Read a Product"""
+        product = ProductFactory()
+        
